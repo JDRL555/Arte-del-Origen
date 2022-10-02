79eab65a3fdd23b8d622f8a30c304e034907a37d
JDRL <joshuleal3@gmail.com>
2022-10-02 12:58:39 -0400

    agregando metatags, minificando js y css, y otras modificaciones extra

--- functions/carrusel.js
+++ functions/carrusel.js
@@ -1,48 +1,16 @@
-const carrusel        = document.querySelector('.carrusel')
-const items           = document.querySelectorAll('.carrusel-item')
-const anterior        = document.querySelector('.anterior')
-const siguiente       = document.querySelector('.siguiente')
-let posicion          = 0
-
-function siguienteImagen(){
-  if(posicion == 0){
-    posicion = -100
-    items.forEach((item)=>{
-      item.style.transform = `translate(${posicion}%)`
-    })
-  }else if(posicion == -100){
-    posicion = -200
-    items.forEach((item)=>{
-      item.style.transform = `translate(${posicion}%)`
-    })
-  }else if(posicion == -200){
-    posicion = 0
-    items.forEach((item)=>{
-      item.style.transform = `translate(${posicion}%)`
-    })
-  }
-}
-
-function anteriorImagen(){
-  if(posicion == 0){
-    posicion = -200
-    items.forEach((item)=>{
-      item.style.transform = `translate(${posicion}%)`
-    })
-  }else if(posicion == -100){
-    posicion = 0
-    items.forEach((item)=>{
-      item.style.transform = `translate(${posicion}%)`
-    })
-  }else if(posicion == -200){
-    posicion = -100
-    items.forEach((item)=>{
-      item.style.transform = `translate(${posicion}%)`
-    })
-  }
-}
-
-setInterval(siguienteImagen, 7000)
-
-siguiente.addEventListener('click', siguienteImagen)
-anterior.addEventListener('click', anteriorImagen)  
+const carrusel=document.querySelector('.carrusel')
+const items=document.querySelectorAll('.carrusel-item')
+const anterior=document.querySelector('.anterior')
+const siguiente=document.querySelector('.siguiente')
+let posicion=0
+function siguienteImagen(){if(posicion==0){posicion=-100
+items.forEach((item)=>{item.style.transform=`translate(${posicion}%)`})}else if(posicion==-100){posicion=-200
+items.forEach((item)=>{item.style.transform=`translate(${posicion}%)`})}else if(posicion==-200){posicion=0
+items.forEach((item)=>{item.style.transform=`translate(${posicion}%)`})}}
+function anteriorImagen(){if(posicion==0){posicion=-200
+items.forEach((item)=>{item.style.transform=`translate(${posicion}%)`})}else if(posicion==-100){posicion=0
+items.forEach((item)=>{item.style.transform=`translate(${posicion}%)`})}else if(posicion==-200){posicion=-100
+items.forEach((item)=>{item.style.transform=`translate(${posicion}%)`})}}
+setInterval(siguienteImagen,7000)
+siguiente.addEventListener('click',siguienteImagen)
+anterior.addEventListener('click',anteriorImagen)
--- functions/mostrar.js
+++ functions/mostrar.js
@@ -1,18 +1,5 @@
-const btn             = document.getElementById('btn')
-const nav             = document.getElementsByClassName('navegacion')
-const puntos          = []
-for(var i = 0; i < 5; i++){
-  puntos.push(document.getElementById(`a${i + 1}`))
-}
-console.log(puntos[0])
-console.log(nav)
-
-btn.addEventListener('click', ()=>{
-  puntos.forEach(punto => {
-    if(punto.style.display == 'none'){
-      punto.style.display = 'block'
-    }else{
-      punto.style.display = 'none'
-    }
-  })
-})
+const btn=document.getElementById('btn')
+const nav=document.getElementsByClassName('navegacion')
+const puntos=[]
+for(var i=0;i<5;i++){puntos.push(document.getElementById(`a${i + 1}`))}
+btn.addEventListener('click',()=>{puntos.forEach(punto=>{if(punto.style.display=='none'){punto.style.display='block'}else{punto.style.display='none'}})})
